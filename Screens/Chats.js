33c4f9113838bac79fff22572b272225b6a49a15
danitlotem <danitlotem@gmail.com>
2022-09-02 19:32:32 +0300

    first commit

---
+++ Screens/Chats.js
@@ -0,0 +1,49 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-alert */
+import React, {useEffect} from 'react';
+import {View, Text} from 'react-native';
+import ChatItem from '../Components/Chat/ChatItem';
+import styles from '../Styles/ChatStyle';
+import UpperBar from '../Components/UpperBar';
+import axios from 'axios';
+import {useSelector, useDispatch} from 'react-redux';
+import {openChats} from '../store/Slices/chatSlice';
+import {shouldRasterizeIOS} from 'deprecated-react-native-prop-types/DeprecatedViewPropTypes';
+const Chat = () => {
+  const myUserId = useSelector(state => state.configuration.userConfig.user_id);
+  const chats = useSelector(state => state.chat.OpenChats);
+  const dispatch = useDispatch();
+
+  const getAllChats = async () => {
+    //FIX ME there is a problem with update list of open chats
+    try {
+      const res = await axios.get(
+        `http://192.168.1.101:3000/chats/${myUserId}`,
+      );
+      dispatch(openChats({OpenChats: res.data}));
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllChats();
+  }, []);
+
+  return (
+    <View style={styles.View.manageChatsContainer}>
+      <View style={styles.View.UpperBarContainer}>
+        <UpperBar />
+      </View>
+      <Text style={styles.Text.title}>my Chats</Text>
+      <View style={styles.View.chatListContainer}>
+        {chats.map((item, index) => (
+          <ChatItem key={index} data={item} />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default Chat;
---
+++ Screens/Menu.js
@@ -0,0 +1,70 @@
+/* eslint-disable react-native/no-inline-styles */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import {SafeAreaView, View, Text, Pressable, Image} from 'react-native';
+import {DrawerItemList} from '@react-navigation/drawer';
+import styles from '../Styles/MenuStyle';
+import {useSelector} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Theme from '../Styles/Theme';
+
+const CustomSidebarMenu = props => {
+  const navigation = useNavigation();
+  const fullName = useSelector(state => state.configuration.fullName);
+  const email = useSelector(state => state.configuration.email);
+  const userConfig = useSelector(state => state.configuration.userConfig);
+  const myStatus = useSelector(state => state.general?.myStatus);
+  const mySearchMode = useSelector(
+    state => state.configuration.filters?.search_mode_filter,
+  );
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        {Object.keys(userConfig).length !== 0 && (
+          <View>
+            <Pressable onPress={() => navigation.navigate('My Profile')}>
+              <Image
+                style={{
+                  height: 110,
+                  width: 110,
+                  borderColor: Theme.secondColor,
+                  borderWidth: 2,
+                  borderRadius: 55,
+                  alignSelf: 'center',
+                }}
+                source={{uri: `data:image/gif;base64,${userConfig.image}`}}
+              />
+            </Pressable>
+            <Text style={styles.fullNameText}>{fullName}</Text>
+            <Text style={styles.emailText}>{email}</Text>
+            <View style={styles.detailsContainer}>
+              <Text style={styles.detailsText}>{mySearchMode}</Text>
+              <View
+                style={{
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                  flexDirection: 'row',
+                }}>
+                <Ionicons
+                  color={'#000000'}
+                  size={15}
+                  name={'chatbubble-ellipses-outline'}
+                />
+                <Text style={styles.detailsText}> {myStatus}</Text>
+              </View>
+            </View>
+          </View>
+        )}
+        {Object.keys(userConfig).length === 0 && (
+          <View>
+            <Text>please log in</Text>
+          </View>
+        )}
+      </View>
+      <DrawerItemList {...props} />
+    </SafeAreaView>
+  );
+};
+
+export default CustomSidebarMenu;
